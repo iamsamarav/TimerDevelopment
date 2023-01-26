9557b67b39a4d5203ade4af0edd8a652f71fa06c
Lilyan Guedes <lilyangabyguedes@gmail.com>
2023-01-26 15:26:28 -0300

    active button start

---
+++ src/script.js
@@ -0,0 +1,36 @@
+const btnStart = document.querySelector(".btn-start")
+
+btnStart.addEventListener('click', () => {
+    const hours = document.querySelector("#hour")
+    const minutes = document.querySelector("#minute")
+    const seconds = document.querySelector("#second")
+
+    let duration = (parseInt(hours.value) * 60 * 60) +
+    (parseInt(minutes.value) * 60) + (parseInt(seconds.value))
+
+    let display = document.querySelector("#timer")
+    timer(duration, display)
+})
+
+const timer = (duration, display) => {
+    let timer = duration
+    let hours, minutes, seconds
+
+    setInterval( () => {
+        hours = Math.floor((timer / 60) / 60)
+        minutes = Math.floor(timer/60)
+        seconds = Math.floor(timer % 60)
+
+        hours = hours < 10 ? '0' + hours : hours
+        minutes = minutes < 10 ? '0' + minutes : minutes
+        seconds = seconds < 10? '0' + seconds : seconds
+
+        display.innerHTML = `${hours} : ${minutes} : ${seconds}`
+
+        timer -= 1
+
+        if (timer < 0) {
+            display.innerHTML = 'Acabou'
+        }
+    }, 1000)
+}
