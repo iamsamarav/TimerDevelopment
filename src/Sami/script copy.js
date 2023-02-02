bbc7241341cab9c00b04d3fc49d6fbcc646b0511
Samara Vitória <samiiivits@gmail.com>
2023-02-02 15:15:01 -0300

    Buttons Start, Pause and Resume finalized

--- src/Sami/script copy.js
+++ src/Sami/script copy.js
@@ -1,38 +1,95 @@
-window.onload=function(){
-    const btnStart = document.querySelector(".btn-start")
+window.onload = function () {
 
-btnStart.addEventListener('click', () => {
-    const hours = document.querySelector("#hour")
-    const minutes = document.querySelector("#minute")
-    const seconds = document.querySelector("#second")
+    const btnStart = document.querySelector("#start")
+    const btnPause = document.getElementById('pause')
+    const btnResume = document.getElementById('resume')
 
-    let duration = (parseInt(hours.value) * 60 * 60) +
-    (parseInt(minutes.value) * 60) + (parseInt(seconds.value))
+    btnStart.addEventListener('click', () => {
+        const hours = document.querySelector("#hour")
+        const minutes = document.querySelector("#minute")
+        const seconds = document.querySelector("#second")
 
-    let display = document.querySelector("#timer")
-    timer(duration, display)
-})
+        let duration = (parseInt(hours.value) * 60 * 60) +
+            (parseInt(minutes.value) * 60) + (parseInt(seconds.value))
 
-const timer = (duration, display) => {
-    let timer = duration
-    let hours, minutes, seconds
+        btnStart.style.display = "none"
+        btnPause.style.display = "block";
 
-    let interval = setInterval(() => {
-        hours = Math.floor((timer / 60) / 60)
-        minutes = Math.floor(timer/60 - (hours * 60)) 
-        seconds = Math.floor(timer % 60)
+        cycle = new timer(duration)
+        cycle.cycleTimer()
+    })
+
+    btnPause.addEventListener('click', () => {
+        let interval = cycle.getInterval()
+        clearInterval(interval)
+        const timerNow = cycle.getTimer()
+        console.log(timerNow)
+        let displayHour = document.querySelector("#display-hour")
+        let displayMinute = document.querySelector("#display-minute")
+        let displaySecond = document.querySelector("#display-second")
+        hours = Math.floor((timerNow / 60) / 60)
+        minutes = Math.floor(timerNow / 60 - (hours * 60))
+        seconds = Math.floor(timerNow % 60)
 
         hours = hours < 10 ? '0' + hours : hours
         minutes = minutes < 10 ? '0' + minutes : minutes
-        seconds = seconds < 10? '0' + seconds : seconds
+        seconds = seconds < 10 ? '0' + seconds : seconds
 
-        display.innerHTML = `${hours}:${minutes}:${seconds}`
+        displayHour.innerHTML = `${hours}`
+        displayMinute.innerHTML = `${minutes}`
+        displaySecond.innerHTML = `${seconds}`
 
-        timer -= 1
+        btnPause.style.display = "none"
+        btnResume.style.display = "flex"
 
-        if (timer < 0) {
-            display.innerHTML = 'Acabou'
-            clearInterval(interval)
+    })
+
+    btnResume.addEventListener('click', () => {
+        duration = cycle.getInterval()
+        btnResume.style.display = "none"
+        btnPause.style.display = "flex"
+        cycle.cycleTimer(duration)
+    })
+
+    class timer {
+        constructor(duration) {
+            this.timer = duration
         }
-    }, 1000)
-}}
+
+        getTimer() {
+            return this.timer
+        }
+
+        getInterval(){
+            return this.interval
+        }
+        cycleTimer() {
+            let displayHour = document.querySelector("#display-hour")
+            let displayMinute = document.querySelector("#display-minute")
+            let displaySecond = document.querySelector("#display-second")
+            let hours, minutes, seconds
+            this.interval = setInterval(() => {
+                hours = Math.floor((this.timer / 60) / 60)
+                minutes = Math.floor(this.timer / 60 - (hours * 60))
+                seconds = Math.floor(this.timer % 60)
+
+                hours = hours < 10 ? '0' + hours : hours
+                minutes = minutes < 10 ? '0' + minutes : minutes
+                seconds = seconds < 10 ? '0' + seconds : seconds
+
+                displayHour.innerHTML = `${hours}`
+                displayMinute.innerHTML = `${minutes}`
+                displaySecond.innerHTML = `${seconds}`
+
+                this.timer -= 1
+
+                if (this.timer < 0) {
+                    clearInterval(this.interval)
+                    btnResume.style.display = "none";
+                    btnPause.style.display = "none";
+                    btnStart.style.display = "flex";
+                }
+            }, 1000)
+        }
+    }
+}
