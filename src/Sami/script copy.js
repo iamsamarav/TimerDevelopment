0d12fc5901019e4fd845fe23c6f1c267aaf30f30
Samara Vitória <samiiivits@gmail.com>
2023-02-05 21:20:05 -0300

    Contributions Samara finalized

--- src/Sami/script copy.js
+++ src/Sami/script copy.js
@@ -7,18 +7,13 @@ window.onload = function () {
     const btnSaved = document.getElementById('saved')
     const sidebar = document.getElementById('sidebar')
     const align = document.getElementById('align')
+    let i = 0
 
     btnStart.addEventListener('click', () => {
-        const hours = document.querySelector("#hour")
-        const minutes = document.querySelector("#minute")
-        const seconds = document.querySelector("#second")
-
-        let duration = (parseInt(hours.value) * 60 * 60) +(parseInt(minutes.value) * 60) + (parseInt(seconds.value))
-
+        const durationStart = getSelectTime()
         btnStart.style.display = "none"
         btnPause.style.display = "block";
-
-        cycle = new timer(duration)
+        cycle = new timer(durationStart)
         cycle.cycleTimer()
     })
 
@@ -26,69 +21,106 @@ window.onload = function () {
         let interval = cycle.getInterval()
         clearInterval(interval)
         const timerNow = cycle.getTimer()
-        console.log(timerNow)
         let displayHour = document.querySelector("#display-hour")
         let displayMinute = document.querySelector("#display-minute")
         let displaySecond = document.querySelector("#display-second")
-        hours = Math.floor((timerNow / 60) / 60)
-        minutes = Math.floor(timerNow / 60 - (hours * 60))
-        seconds = Math.floor(timerNow % 60)
-
-        hours = hours < 10 ? '0' + hours : hours
-        minutes = minutes < 10 ? '0' + minutes : minutes
-        seconds = seconds < 10 ? '0' + seconds : seconds
-
-        displayHour.innerHTML = `${hours}`
-        displayMinute.innerHTML = `${minutes}`
-        displaySecond.innerHTML = `${seconds}`
-
+        const Hours = getHours(timerNow)
+        const Minutes = getMinutes(timerNow)
+        const Seconds = getSeconds(timerNow)
+        displayHour.innerHTML = `${Hours}`
+        displayMinute.innerHTML = `${Minutes}`
+        displaySecond.innerHTML = `${Seconds}`
         btnPause.style.display = "none"
         btnResume.style.display = "flex"
-
     })
 
     btnResume.addEventListener('click', () => {
-        duration = cycle.getInterval()
+        durationResume = cycle.getInterval()
         btnResume.style.display = "none"
         btnPause.style.display = "flex"
-        cycle.cycleTimer(duration)
+        cycle.cycleTimer(durationResume)
     })
 
     btnAdd.addEventListener('click', () => {
-        let i = 0
-        const hours = document.querySelector("#hour")
-        const minutes = document.querySelector("#minute")
-        const seconds = document.querySelector("#second")
-
-        let duration = (parseInt(hours.value) * 60 * 60) +(parseInt(minutes.value) * 60) + (parseInt(seconds.value))
-
-        let displayhours = Math.floor((duration / 60) / 60)
-        let displayminutes = Math.floor( duration/ 60 - (displayhours * 60))
-        let displayseconds = Math.floor( duration % 60)
-
+        const durationAdd = getSelectTime()
+        const displayHours = getHours(durationAdd)
+        const displayMinutes = getMinutes(durationAdd)
+        const displaySeconds = getSeconds(durationAdd)
         const elementSaved = document.createElement('button')
         elementSaved.style.backgroundColor = "#34344e";
         elementSaved.style.display = "block";
         elementSaved.style.marginTop = "0.4rem";
         elementSaved.style.marginBottom = "0.4rem"
         elementSaved.style.backgroundColor = 'white'
+        elementSaved.style.height = '2rem'
+        elementSaved.style.width = '100%'
         elementSaved.style.border = 'none'
-        elementSaved.innerHTML = `${displayhours} : ${displayminutes} : ${displayseconds}`
+        elementSaved.innerHTML = `${displayHours} : ${displayMinutes} : ${displaySeconds}`
         sidebar.insertBefore(elementSaved, sidebar.childNodes[i])
+        elementSaved.setAttribute("id", i.valueOf())
         i++
-
         align.style.display = 'flex';
         align.style.justifyContent = 'center';
         btnSaved.style.display = "block";
     })
 
-    btnSaved.addEventListener('click', () =>{
-        sidebar.style.display = "flex"
-        sidebar.style.flexDirection = "column"
-        sidebar.style.alignItems = "center"
-        sidebar.style.justifyContent = "flex-start"
-        
+    btnSaved.addEventListener('click', () => {
+        if (sidebar.style.display == 'none') {
+            sidebar.style.display = "flex"
+            sidebar.style.flexDirection = "column"
+            sidebar.style.alignItems = "center"
+            sidebar.style.justifyContent = "flex-start"
+        } else {
+            sidebar.style.display = 'none'
+        }
     })
+
+    sidebar.addEventListener('click', (e) => {
+        let element = e.target
+        let id = element.id
+        console.log(id)
+        let hours = sidebar.childNodes[id].textContent.substring(0, 2)
+        let minutes = sidebar.childNodes[id].textContent.substring(5, 7)
+        let seconds = sidebar.childNodes[id].textContent.substring(10, 13)
+        hours = parseInt(hours)
+        minutes = parseInt(minutes)
+        seconds = parseInt(seconds)
+
+        let displayHour = document.getElementById("hour")
+        let displayMinute = document.getElementById("minute")
+        let displaySecond = document.getElementById("second")
+
+        displayHour.value = hours
+        displayMinute.value = minutes
+        displaySecond.value = seconds
+    })
+
+    function getHours(time) {
+        hours = Math.floor((time / 60) / 60)
+        hours = hours < 10 ? '0' + hours : hours
+        return hours
+    }
+
+    function getMinutes(time) {
+        minutes = Math.floor(time / 60 - (hours * 60))
+        minutes = minutes < 10 ? '0' + minutes : minutes
+        return minutes
+    }
+
+    function getSeconds(time) {
+        seconds = Math.floor(time % 60)
+        seconds = seconds < 10 ? '0' + seconds : seconds
+        return seconds
+    }
+
+    function getSelectTime() {
+        const hours = document.querySelector("#hour")
+        const minutes = document.querySelector("#minute")
+        const seconds = document.querySelector("#second")
+
+        let duration = (parseInt(hours.value) * 60 * 60) + (parseInt(minutes.value) * 60) + (parseInt(seconds.value))
+        return duration
+    }
 
     class timer {
         constructor(duration) {
@@ -99,7 +131,7 @@ window.onload = function () {
             return this.timer
         }
 
-        getInterval(){
+        getInterval() {
             return this.interval
         }
         cycleTimer() {
